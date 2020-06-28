bdaad220e62d16c49f12b72daa1960fe970ff19d
mateuszklinowski <klinowski.mateusz@gmail.com>
2020-06-28 15:56:48 +0200

    Initial commit + project setup

---
+++ src/components/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './../styles/index.scss';
+
+export const App: React.FunctionComponent = () => {
+    return <h1>Chat app TODO</h1>;
+};
---
+++ webpack.dev.ts
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+});
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [htmlPlugin],
+};
+
+export default config;
